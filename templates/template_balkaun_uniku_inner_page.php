6f06a2f668a8a85727ecb63f2289d3b700ec3ee5
Helder Menezes <helder.menezes@r2m.tl>
2023-05-12 10:53:38 +0900

    Add condicional background for balkaun innerpage header

--- templates/template_balkaun_uniku_inner_page.php
+++ templates/template_balkaun_uniku_inner_page.php
@@ -5,18 +5,17 @@
 include "balkaun_uniku_header.php";
 
 $post_image_feature = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
-
-if (has_post_thumbnail($post) && $post_image_feature != null) {
-    $post_image_feature_url = $post_image_feature;
-} else {
-    $post_image_feature_url = show_default_background_image(); //get_template_directory_uri() . "/img/wave.png";
+$style = '';
+if (!$post_image_feature) {
+    $style='background:linear-gradient(0deg, rgba(14, 29, 52,0) 0%, rgba(150, 8, 7 ,.57) 100%)';
+}else{
+    $style='background-image:url('.$post_image_feature.')';
 }
-
 ?>
 
 <main id="main">
     <div class="breadcrumbs">
-        <div class="page-header d-flex align-items-center" style="background-image: url(<?= $post_image_feature?>);">
+        <div class="page-header d-flex align-items-center" style="<?= $style?>">
             <div class="container position-relative">
                 <div class="row d-flex justify-content-center">
 
