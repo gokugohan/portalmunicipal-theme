eb194a277bf2a00e76d7287aa32b23de2e2e1301
Helder Menezes <helder.menezes@r2m.tl>
2023-05-12 10:48:32 +0900

    Add feature image/color to header

--- templates/template_balkaun_uniku_inner_page.php
+++ templates/template_balkaun_uniku_inner_page.php
@@ -3,11 +3,20 @@
   Template Name: Balkaun Úniku Page - Inner page
  */
 include "balkaun_uniku_header.php";
+
+$post_image_feature = wp_get_attachment_url(get_post_thumbnail_id($post->ID));
+
+if (has_post_thumbnail($post) && $post_image_feature != null) {
+    $post_image_feature_url = $post_image_feature;
+} else {
+    $post_image_feature_url = show_default_background_image(); //get_template_directory_uri() . "/img/wave.png";
+}
+
 ?>
 
 <main id="main">
     <div class="breadcrumbs">
-        <div class="page-header d-flex align-items-center" style="background-image: url(<?= $hero_background_image?>);">
+        <div class="page-header d-flex align-items-center" style="background-image: url(<?= $post_image_feature?>);">
             <div class="container position-relative">
                 <div class="row d-flex justify-content-center">
 
